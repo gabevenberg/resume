7d55e5edca41749670c127f8bcca5c9e77ed3997
Gabe Venberg <gabevenberg@gmail.com>
2026-04-30 14:58:04 +0200

    flake update

--- flake.nix
+++ flake.nix
@@ -1,22 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "Devshell for working on my resume.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in
-      with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [texliveFull];
-        };
-      });
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system:
+        function {
+          pkgs = import nixpkgs {inherit system;};
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          texliveFull
+          just
+        ];
+      };
+    });
+  };
 }
